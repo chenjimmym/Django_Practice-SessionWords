16bad60f435dea94e4ae5b6789e203b0b7d2e941
Jimmy <chen.jimmym@gmail.com>
2017-10-17 20:25:53 -0700

    done-style can be updated

--- apps/words/urls.py
+++ apps/words/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'form_processing', views.form_processing),
+    url(r'clear_session', views.clear_session),
 ]
--- apps/words/views.py
+++ apps/words/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, HttpResponse
-
+from time import gmtime, strftime
 # Create your views here.
 def index(request):
+    print strftime("%Y-%m-%d %H:%M:%S", gmtime())
     return render(request, 'words/index.html')
 
 def form_processing(request):
@@ -10,8 +11,12 @@ def form_processing(request):
             request.session['all_inputs'] = []
         else:
             request.session['all_inputs'] = request.session['all_inputs']
-        temp = {'word':request.POST['input_word'],'color':request.POST['color'],'font':request.POST['big_font']}
+        temp = {'word':request.POST['input_word'],'color':request.POST['color'],'font':request.POST['big_font'], 'time':strftime("%Y-%m-%d %H:%M:%S", gmtime())}
         request.session['all_inputs'].append(temp)
     	return redirect("/")
     else:
     	return redirect("/")
+
+def clear_session(request):
+    request.session['all_inputs'] = []
+    return redirect("/")
